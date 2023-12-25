bd97087b9191288fc9ec458f103eb89e690e06b7
gharahld <gharahld19@yahoo.com>
2023-12-25 12:10:58 -0500

    adding db

---
+++ db.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=True)
+
+
+with app.app_context():
+    db.create_all()
--- login.py
+++ login.py
@@ -1,22 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('login.html')
+from db import app, db, bcrypt, User
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the user is already in the database
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return 'Username already exists. Please choose a different one.'
+
+        # Hash the password before storing it
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # Create a new user
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Redirect to the login page after successful registration
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return redirect(url_for('home'))
-
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    return render_template('login.html')
 
 @app.route('/home')
 def home():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
